94d779a27126345dfb67c9ded7b2b33b41e7bf7d
Roy <roygourab2706@gmail.com>
2026-08-20 14:58:24 +0530

    Simple show case of Conditional Statement.

---
+++ Conditional_statement.cpp
@@ -0,0 +1,24 @@
+// Conditional statement.
+
+// The work of conditional statement is to check the condition and accordingly print out..
+// If the condition is valid, then it will print the block of 'if', else it will print 'else' block..
+
+
+// Simple example.
+#include<iostream>
+
+int main()
+{
+    int x = 10;
+
+    if(x == 10)
+    {
+        std::cout<<"Yes!!";
+    }
+    else
+    {
+        std::cerr<<"No!!";
+    }
+
+    return 0;
+}
