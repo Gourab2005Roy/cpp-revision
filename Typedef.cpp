420fa25dacc96d26ee325c432347ae49fa4a926e
Roy <roygourab2706@gmail.com>
2026-08-20 14:37:21 +0530

    Learning and understanding the concept of 'Typedef'.

--- Typedef.cpp
+++ Typedef.cpp
@@ -1,3 +1,26 @@
 // Typedef..
+// Here, we gonna learn the working principle of 'Typedef'...
 
-// Here, we gonna learn the working 
+// There are times, where we do declare variables and work with it.
+// But, gets no idea about the used variable while reviewing our code after days..
+// So to make sure, there happens nothing such, we use 'Typedef' and make our work more easier....
+
+// Way of reading a 'typedef'
+// typedef <existing_type> <new_name>;
+// Read it right to left: "new_name" is a typedef for 'existing_type'
+
+
+#include<iostream>
+
+typedef int marks;
+
+int main()
+{
+    marks m1, m2; // Here, 'm1' 'm2' are the variables, and the data-type of the variables is the same as that of the data-type that's being used while describing the typedef..
+    
+    m1 = 10;  // Initialzing value in the variable..
+
+    std::cout<<m1;
+
+    return 0;
+}
