8de5bf9c22e7868b2691992207c18a1fc979ad08
Roy <roygourab2706@gmail.com>
2026-08-21 15:48:08 +0530

    Printing the upper triangle

---
+++ upper_triangle.cpp
@@ -0,0 +1,25 @@
+// Upper triangle.
+
+/*
+* * * *
+* * *
+* *
+*
+*/
+
+#include<iostream>
+
+int main()
+{
+    char a = '*';
+
+    for(std::size_t i = 0; i<4; i++)
+    {
+        for(std::size_t j = 0; (j+i)<4; j++)
+        {
+            std::cout<<a<<" ";
+        }
+        std::cout<<"\n";
+    }
+    return 0;
+}
