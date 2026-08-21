859d6ddda86e1ca844d9a7db4f5f5d06ff736666
Roy <roygourab2706@gmail.com>
2026-08-21 15:36:18 +0530

    Just a simple pattern printing.

---
+++ simple_pattern.cpp
@@ -0,0 +1,18 @@
+// Printing a simple pattern in multi-dimensional.
+
+#include<iostream>
+
+int main()
+{
+    char a = '*';
+    for(std::size_t i = 0; i<4; i++)
+    {
+        for(std::size_t j = 0; j<4; j++)
+        {
+            std::cout<<a<<" ";
+        }
+        std::cout<<"\n";
+    }
+
+    return 0;
+}
