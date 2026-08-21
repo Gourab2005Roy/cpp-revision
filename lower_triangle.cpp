746c821e4db4bff5c4ec480635ad49c6008ed922
Roy <roygourab2706@gmail.com>
2026-08-21 15:41:21 +0530

    Printing lower triangle using '*'

--- lower_triangle.cpp
+++ lower_triangle.cpp
@@ -1,3 +1,25 @@
 // Lower Triangle.
 
+/*
+*
+* * 
+* * *
+* * * *
+*/
+
 #include<iostream>
+
+int main()
+{
+    char a = '*';
+
+    for(std::size_t i = 0; i<4; i++)
+    {
+        for(std::size_t j = 0; j<=i; j++)
+        {
+            std::cout<<a;
+        }
+        std::cout<<"\n";
+    }
+    return 0;
+}
