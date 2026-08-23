82d23176ec48bd90292413ac09d4a117f57f1613
Roy <roygourab2706@gmail.com>
2026-08-23 14:29:24 +0530

    Reason for using the conditions.

--- pyramid.cpp
+++ pyramid.cpp
@@ -15,11 +15,11 @@ int main()
 
     for(std::size_t i = 0; i<n; i++)
     {
-        for(std::size_t j = 0; j<(n-i-1); j++)
+        for(std::size_t j = 0; j<(n-i-1); j++) // The condition here tells, how many times, the ' ' should be printed.
         {
             std::cout<<" ";
         }
-        for(std::size_t k = 0; k<(2*i+1); k++)
+        for(std::size_t k = 0; k<(2*i+1); k++) // The condition here tells us, how many times, '*' should be printed.
         {
             std::cout<<"*";
         }
