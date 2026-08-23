e5a8ba6edd386c9a774b9f43c912c2406cf769fa
Roy <roygourab2706@gmail.com>
2026-08-23 14:25:33 +0530

    Understood how pointers work.

--- pointers.cpp
+++ pointers.cpp
@@ -1,12 +1,23 @@
-#include <iostream>
+// Pointers.
 
-int main() {
-    int number = 10;
-    int* ptr = &number;
+// Here, we will understand how, pointers work.
 
-    std::cout << "Value: " << number << std::endl;
-    std::cout << "Address: " << ptr << std::endl;
-    std::cout << "Value via pointer: " << *ptr << std::endl;
+
+
+#include<iostream>
+
+int main()
+{
+    int x = 10;
+    int *p; // Initializing a pointer.
+
+    p = &x; // pointing the pointer in the address of 'x'.
+
+    std::cout<<x<<"\n"; // It prints the value inside 'x'.
+    std::cout<<p<<"\n"; // It prints the address of 'x' that's been stored in 'p'.
+    std::cout<<*p<<"\n"; // It prints the value of the address in which 'p' is focusing.
+    std::cout<<&p<<"\n"; // It prints the address value of 'p'.
+    std::cout<<&x; // It prints the address value of 'x';
 
     return 0;
 }
