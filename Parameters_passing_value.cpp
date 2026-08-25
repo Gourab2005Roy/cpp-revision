bb7703181534c2cf1ed6bae692b0ce6792f5b957
Roy <roygourab2706@gmail.com>
2026-08-25 10:35:26 +0530

    Here, we saw the influence of passing_by_value in function and also saw whether it can make changes in the original/real value.

---
+++ Parameters_passing_value.cpp
@@ -0,0 +1,26 @@
+// Pass by value. OR Call by value.
+
+// Means passing value to the function.
+// The value that's being passed to the function has no influence on the main value.
+
+
+#include<iostream>
+
+void swap(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+    std::cout<<a<<" "<<b<<" is the swapped one.\n";
+}
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+
+    swap(a, b);
+    std::cout<<a<<" "<<b<<" is the original one.\n";
+
+    return 0;
+}
