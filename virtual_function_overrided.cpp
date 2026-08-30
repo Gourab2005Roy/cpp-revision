9bbd59ce7aa799243389612abdbb26dc4b4c7750
Roy <roygourab2706@gmail.com>
2026-08-30 12:14:28 +0530

    Here, we understood the concept of virtual function overrided and runtime polymorphism.

---
+++ virtual_function_overrided.cpp
@@ -0,0 +1,43 @@
+// Virtual function overrided.
+
+// Using a base class pointer derived class object, when we call a over-rided function, then base-class function will be called.
+// But, we want to call the function that is in the derive class, then we need to introducte the concept of 'virtual'.
+
+// As / Since, we used 'virtual' the pointer is not pointing to that function and it is pointing to the over-rided function.
+// This is the type of ambiguity, we face we use 'base class pointer derived object'.
+// In C++ 'virtual', is used to enable polymorphism, not just for clarity. It's about allowing functions to be overridden in derived classes, which helps in run-time behaviour rathr than just making the code cleaner.
+
+// This is runtime polymorphism.
+
+
+#include<iostream>
+
+class Base
+{
+    public:
+    virtual void fun()
+    {
+        std::cout<<"First function.\n";
+    }
+};
+
+class Derived : public Base
+{
+    public:
+    void fun()
+    {
+        std::cout<<"Second function.\n";
+    }
+};
+
+int main()
+{
+    Base *p;
+    p = new Derived();
+    
+    p->fun();
+
+    delete p;
+    p = nullptr;
+    return 0;
+}
