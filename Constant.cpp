e1ac43c9a4ea7ac7d2d1134110a701a564d14e7c
Roy <roygourab2706@gmail.com>
2026-09-01 16:26:05 +0530

    Here, we saw what does 'constant' do a variable.

---
+++ Constant.cpp
@@ -0,0 +1,14 @@
+// Constant.
+// From the above word, we can understand that with what we are going to dealing.
+
+
+#include<iostream>
+
+int main()
+{
+    const int x = 10;  // Here, we are going to make the variable constant.
+    // ++x;  // The variable being constant, we cannot do any type of modification within it.
+    std::cout<<x;
+    
+    return 0;
+}
